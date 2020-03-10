cf7dc0e4dc3b663d2617c5089ea875adba9691ec
MAALONSO <miguel.coba@gnp.com.mx>
2020-03-10 15:15:52 -0600

    Creando el repositorio JPA para los usuarios

---
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -0,0 +1,33 @@
+package com.formacionbdi.springboot.app.usuarios.models.dao;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+
+/**
+ * Interface que hereda para realizar la gestion hacia la BD.
+ */
+public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
+
+	/**
+	 * Realiza una consulta JPA a la tabla Usuario dependiendo de su UserName.
+	 * Utiliza las palabras clave de JPA por Method.
+	 * 
+	 * @param String username
+	 * @return Class Usuario
+	 */
+	public Usuario findByUsername(String username);
+
+	/**
+	 * Methodo que busca un usuario. Se implementa con la etiqueta @Query y se
+	 * define el query. La consulta se realiza a nivel de Entity no a nivel de las
+	 * tablas.
+	 * 
+	 * @param username
+	 * @return
+	 */
+	@Query("select u from Usuario u where u.username=?1")
+	public Usuario obtenerPorUsername(String username);
+
+}
