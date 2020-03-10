259ed9132d1665b7949179f2bf5b5fe7200d7e9c
MAALONSO <miguel.coba@gnp.com.mx>
2020-03-10 16:19:39 -0600

    Implementando un repositorio rest para el CRUD

--- src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -2,12 +2,14 @@ package com.formacionbdi.springboot.app.usuarios.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
 
 /**
  * Interface que hereda para realizar la gestion hacia la BD.
  */
+@RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
 	/**
