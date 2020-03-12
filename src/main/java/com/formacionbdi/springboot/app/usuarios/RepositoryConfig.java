b9e2dd4cd6392d2a85fc5412e80815a4ae6bb6e8
MAALONSO <miguel.coba@gnp.com.mx>
2020-03-12 16:45:08 -0600

    Configurando para exponer id en la respuesta JSON

---
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/RepositoryConfig.java
@@ -0,0 +1,21 @@
+package com.formacionbdi.springboot.app.usuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.formacionbdi.springboot.app.usuarios.models.entity.Role;
+import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+
+/**
+ * Clase config: Se realiza la configuración para mostrar los Id en el JSON cuando se consume el microservicio.
+ */
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+
+}
