87c120f7ba659cd8dc3386d6595cc08e116de0fe
MAALONSO <miguel.coba@gnp.com.mx>
2020-04-21 13:35:00 -0500

    Configurando PostgresSQL

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$2EVSZo/mRPl6h7hW1JfJq.gL3icedwoh/uHtfqRZhOC2xizffxHWu',1, 'Andres', 'Guzman','profesor@bolsadeideas.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$KD6FI2O5t0RLE/j9S20wBeJNz3xvNWWXNkd2sTznBXV34fwshyuOi',1, 'John', 'Doe','jhon.doe@bolsadeideas.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('andres','$2a$10$2EVSZo/mRPl6h7hW1JfJq.gL3icedwoh/uHtfqRZhOC2xizffxHWu',true, 'Andres', 'Guzman','profesor@bolsadeideas.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin','$2a$10$KD6FI2O5t0RLE/j9S20wBeJNz3xvNWWXNkd2sTznBXV34fwshyuOi',true, 'John', 'Doe','jhon.doe@bolsadeideas.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
